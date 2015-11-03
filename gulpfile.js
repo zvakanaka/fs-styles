b771ac53934a7ee0172b46e54b656780449605e0
Steven Lambert <steven.lambert@ldschurch.org>
2015-11-03 10:26:00 -0700

    auto build comment header for compiled css files

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@ var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
 var size = require('gulp-size');
+var insert = require('gulp-insert');
+var package = require('package.json');
+
+var header = '/*\n' +
+             ' * FamilySearch Styles\n' +
+             ' * https://github.com/fs-webdev/fs-styles\n' +
+             ' * v' + package.version + ' | MIT\n' +
+             ' */\n';
 
 gulp.task('build', function() {
   return gulp.src('assets/css/familysearch-styles.styl')
     .pipe(stylus())
+    .pipe(insert.prepend(header))
     .pipe(gulp.dest('dist'))
     .pipe(minify())
+    .pipe(insert.prepend(header))
     .pipe(rename(function(path) {
       path.basename += '.min';
     }))
